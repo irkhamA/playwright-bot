8f06ba8b56e8d5f1714448b04e7a236c15ab1048
Irkham Agung Karunia <70405316+irkhamA@users.noreply.github.com>
2024-08-13 20:10:21 +0700

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Open Page and reload it', async ({ page }) => {
   
-
-  for (let i = 1; i <= 10000; i++) {
+  //change loop as much as you want
+  for (let i = 1; i <= 10; i++) {
     console.log(`Reloading page - Iteration ${i}`);
     await page.goto('irkhamA');
 
